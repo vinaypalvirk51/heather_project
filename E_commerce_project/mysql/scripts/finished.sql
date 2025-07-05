7ef84f8c7083bbc7fc44db714853f4bf307697bb
Sukh <sukhdeepsona93@gmail.com>
2025-07-05 17:36:38 -0400

    Ignore E_commerce_project folder

--- E_commerce_project/mysql/scripts/finished.sql
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# signal that the import has finsihed
-touch /tmp/finished
--- E_commerce_project/shipping/src/main/java/com/instana/robotshop/shipping/CodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.instana.robotshop.shipping;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CodeRepository extends PagingAndSortingRepository<Code, Long> {
-    // Inherits all CRUD methods
-}
